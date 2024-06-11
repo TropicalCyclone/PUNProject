7a40678bb73f6264695bef742c72726f74784693
DESKTOP-VACFDVM\USER-PC <logdatkyle@gmail.com>
2024-06-12 00:49:09 +0800

    new_player_controller
    -removed unecessary stuff in items

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : BaseItem, IPaintMaterial
+public class Item : BaseItem
 {
-    public void SetMaterial(Material material)
-    {
-    }
+
 }
